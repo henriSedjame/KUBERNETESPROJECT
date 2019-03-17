828481ef442df08b66dd4103ceb7af0508191cd2
henriSedjame <sedhjo@gmail.com>
2019-03-17 19:07:10 +0100

    Initial commit

---
+++ INSTALL_TOOLS/bootstrap_master.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Initialisation du cluster
+echo "[TASK 1] Initialisation du cluster"
+kubeadm init --pod-network-cidr=192.168.0.0/16 # add --apiserver-advertise-address="ip" if you want to use a different IP address than the main server IP
+export KUBECONFIG=/etc/kubernetes/admin.conf
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+
