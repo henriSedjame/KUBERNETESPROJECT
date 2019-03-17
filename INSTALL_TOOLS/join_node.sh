fd025f3746117a5a4de298a6be8a69c867cc01db
HSE <32457325+henriSedjame@users.noreply.github.com>
2019-03-17 19:43:52 +0100

    Create join_node.sh

---
+++ INSTALL_TOOLS/join_node.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Join
+kubeadm join 192.168.0.25:6443 --token 6foidq.5g0hio7sahoyqek8 --discovery-token-ca-cert-hash sha256:723264b171032d05f059cdace2a21ab318ddf3345232c02dcbbf7b6af57646a1
