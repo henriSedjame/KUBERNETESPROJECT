710fbe804822bfb851251b45d0c23f3dbcddc903
henriSedjame <sedhjo@gmail.com>
2019-03-21 19:39:47 +0100

    modif

--- INSTALL_TOOLS/bootstrap_master.sh
+++ INSTALL_TOOLS/bootstrap_master.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Désactiver swap
-swapoff -a
+sudo swapoff -a
 # Initialisation du cluster
 echo "[TASK 1] Initialisation du cluster"
-kubeadm init --pod-network-cidr=192.168.0.0/16 # add --apiserver-advertise-address="ip" if you want to use a different IP address than the main server IP
+kubeadm init --pod-network-cidr=192.168.0.0/16  --apiserver-advertise-address=192.168.0.19
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 #export KUBECONFIG=/etc/kubernetes/admin.conf
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 
